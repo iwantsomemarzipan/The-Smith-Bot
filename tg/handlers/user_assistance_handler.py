cd3d1c649a42bf8d79ebd334d9033885235131e0
iwantsomemarzipan <novoselova297@gmail.com>
2024-03-23 22:40:50 +0300

    changed link to repo

--- tg/handlers/user_assistance_handler.py
+++ tg/handlers/user_assistance_handler.py
@@ -8,13 +8,13 @@ assistance_router: Router = Router()
 
 url_button: InlineKeyboardButton = InlineKeyboardButton(
     text='"There is a quick and easy way" you say',
-    url='https://private-spruce-237.notion.site/7364ad0de7de4a279856a17f687b9da8?pvs=4')
+    url='https://github.com/iwantsomemarzipan/The-Smith-Bot')
 
 keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(
     inline_keyboard=[[url_button]])
 
 
-# Команда для получения ссылки на страницу в ноушен
+# Команда для получения ссылки репозитория
 @assistance_router.message(Command(commands='info'))
 async def process_info_command(message: Message):
     await message.answer(LEXICON['/info'], reply_markup=keyboard)
